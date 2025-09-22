f2bb8768150d47a6d077d4a71b5692616ac96028
vinitha1710 <58410540+vinitha1710@users.noreply.github.com>
2025-09-22 19:56:25 -0400

    Inject the forecast HTML from JS

--- src/app.js
+++ src/app.js
@@ -55,7 +55,32 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="weather-forecast-day">
+            <div class="weather-forecast-date">${day}</div>
+            <div class="weather-forecast-icon">☀️</div>
+            <div class="weather-forecast-temp">
+              <div class="weather-forecast-temperature">
+                <strong>73°</strong>
+              </div>
+              <div class="weather-forecast-temperature">| 69°</div>
+            </div>
+          </div>
+          `;
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Lisbon");
+displayForecast();
